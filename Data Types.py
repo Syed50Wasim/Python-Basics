46b16f34a9912c37f6ba1478ba7d722395364c23
Syed Wasimuddin <95518763+Syed50Wasim@users.noreply.github.com>
2023-07-09 19:48:24 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Fundamental Data Types of Python
+
+# INTEGERS
+
+# In[1]:
+
+
+a = 7
+type(a)
+
+
+# In[2]:
+
+
+b = 77777
+type(b)
+
+
+# FLOAT
+
+# In[3]:
+
+
+pi = 3.14
+type(pi)
+
+
+# In[4]:
+
+
+x = 2.0
+type(x)
+
+
+# COMPLEX NUMBERS
+
+# In[5]:
+
+
+a = 2 + 3j
+type(a)
+
+
+# DYNAMIC TYPING
+
+# In[6]:
+
+
+a = 22
+type(a)
+
+
+# In[7]:
+
+
+a = 22.2
+type(a)
+
+
+# In[8]:
+
+
+a = 'Wasim'
+type(a)
+
+
+# In[9]:
+
+
+print(type(a))
+
+
+# STRONG TYPING
+
+# In[10]:
+
+
+"Day" + 1
+
+
+# In[11]:
+
+
+"Day" + "1"
+
+
+# In[12]:
+
+
+"33" + "3"
+
+
+# In[14]:
+
+
+33 + 3
+
+
+# In[15]:
+
+
+11.1 + 1
+
+
+# BOOLEAN DATA TYPE
+
+# In[2]:
+
+
+one = True
+two = False
+print(type(one))
+
+
+# In[3]:
+
+
+print(type(two))
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+print("HI WELCOME TO THE PYTHON CLASS")
+
+
+# In[6]:
+
+
+Language = "pyhton"
+print("HI Welcome to class of "+Language+" Language")
+
+
+# FORMAING STRING MEATHOD
+
+# In[7]:
+
+
+Language = "Python"# STANDARD FORMAT
+print(f"Hi Welcome to the {Language} Language")
+
+
+# In[8]:
+
+
+language = "JavaScript"
+print(f"Hi Welcome to the {language} Language")
+
+
+# In[ ]:
+
+
+
+
